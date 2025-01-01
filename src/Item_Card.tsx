657c226d3232df5df8d0e28ae8a5930ecb671564
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2025-01-01 09:51:48 -0500

    Initialized Item Card with API functionality

--- src/Item_Card.tsx
+++ src/Item_Card.tsx
@@ -1,3 +1,26 @@
+import { useState, useEffect } from 'react';
+import './Item_Card.css'
+
 export default function Item_Card() {
-    return <p>hi</p>
+    const [itemData, setItemData] = useState(null);
+
+    useEffect( 
+    () => {
+        fetch('https://fakestoreapi.com/products/1')
+        .then(res=>res.json())
+        .then((data)=>setItemData(data))
+    }, [])
+
+    if (itemData) {
+        return <>
+        <div className="Card">
+        <h3>{itemData.title}</h3>
+        <p>{itemData.price}</p>
+        <img src={itemData.image}></img>
+        </div>
+    </>
+    }
+
+    return null;
+
 }
--- src/Shop_Page.tsx
+++ src/Shop_Page.tsx
@@ -1,12 +1,13 @@
 import Item_Card from './Item_Card.tsx'
 
 function Shop_Page(props) {
+
     return (
         <>
         <button onClick={() => props.setpageNum(0)}>Home Page</button>
         <p className="PageName">Shop Page</p>
         <div className='CardContainer'
-        style={{border: '1px solid black', height: '500px'}}
+        style={{border: '1px solid black', height: '500px', padding: '5px'}}
         >
             <Item_Card />
         </div>
