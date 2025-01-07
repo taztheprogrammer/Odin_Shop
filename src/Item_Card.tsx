1241ea57d18443dce9685b81d09c474c02b8cee1
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2025-01-07 09:51:50 -0500

    importing multiple items

--- src/Item_Card.tsx
+++ src/Item_Card.tsx
@@ -1,26 +1,15 @@
-import { useState, useEffect } from 'react';
 import './Item_Card.css'
 
-export default function Item_Card() {
-    const [itemData, setItemData] = useState(null);
+export default function Item_Card( { itemData } ) {
 
-    useEffect( 
-    () => {
-        fetch('https://fakestoreapi.com/products/1')
-        .then(res=>res.json())
-        .then((data)=>setItemData(data))
-    }, [])
 
-    if (itemData) {
-        return <>
-        <div className="Card">
+
+    return <div className="Card">
         <h3>{itemData.title}</h3>
         <p>{itemData.price}</p>
-        <img src={itemData.image}></img>
-        </div>
-    </>
-    }
-
-    return null;
+        <img src={itemData.image} alt={itemData.title}/>
+    </div>
+    
+    ;
 
 }
--- src/Shop_Page.tsx
+++ src/Shop_Page.tsx
@@ -1,15 +1,29 @@
+import { useState, useEffect } from 'react'
 import Item_Card from './Item_Card.tsx'
 
 function Shop_Page(props) {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products?limit=5')
+        .then((res) => res.json())
+        .then((data) =>setItems(data))
+
+    })
 
     return (
         <>
         <button onClick={() => props.setpageNum(0)}>Home Page</button>
         <p className="PageName">Shop Page</p>
         <div className='CardContainer'
-        style={{border: '1px solid black', height: '500px', padding: '5px'}}
-        >
-            <Item_Card />
+        style={{border: '1px solid black', height: '500px', padding: '5px'}}>
+            {items.map((item) => (
+                <Item_Card key={item.id} itemData={item} />
+            ))}
+
+
+
+            
         </div>
         </>
         
